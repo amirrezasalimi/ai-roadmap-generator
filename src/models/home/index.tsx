ef4d82be68996e8e94fb567f7f33bfe41b6ae529
damirchi <damirchi@pro.ai>
2023-01-28 09:21:30 -0800

    setup flow, new flow, home page

---
+++ src/models/home/index.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return(
+    <div>
+      home 
+    </div>
+  )
+};
+export default Home;
--- src/pages/panel/new-flow/index.tsx
+++ src/pages/panel/new-flow/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import { Button } from '@nextui-org/react';
+import NewFlow from "../../../models/new-flow";
 
 const FlowPage = () => {
   return (
@@ -8,7 +8,7 @@ const FlowPage = () => {
       <Head>
         <title>ai roadmap</title>
       </Head>
-      <Button> flow </Button>
+      <NewFlow />
     </>
   );
 };
