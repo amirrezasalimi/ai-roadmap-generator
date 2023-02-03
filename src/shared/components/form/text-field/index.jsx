883948cbdafcf2d2a3eb26d8476406ed428ce947
damirchi <damirchi@pro.ai>
2023-02-03 07:01:29 -0800

    override input component/ refactor text field form

--- src/shared/components/form/text-field/index.jsx
+++ src/shared/components/form/text-field/index.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
-import { Input } from '@nextui-org/react';
+import Input from '../../input';
 
 
-const TextField = ({ name, commaSeprated, ...rest }) => {
+const TextField = ({ name, ...rest }) => {
   const methods = useFormContext();
   const { getValues, setValue, watch, formState, clearErrors } = methods;
   const isInvalid = formState?.errors?.[name];
+
   useEffect(() => {
     if (getValues(name)) {
       clearErrors(name);
@@ -19,7 +20,7 @@ const TextField = ({ name, commaSeprated, ...rest }) => {
       onChange={(e) => {
           setValue(name, e.target.value);
       }}
-      value={commaSeprated ? showValue : getValues(name)}
+      value={getValues(name)}
       {...rest}
     />
   );
---
+++ src/shared/components/input/index.js
@@ -0,0 +1,11 @@
+import { Input as NextInput, styled } from '@nextui-org/react';
+
+
+
+const Input = styled(NextInput, {
+      "& div": {
+        borderRadius: 5,
+      },
+    });
+
+export default Input;
