7809fe0c024b921a1e9352f600e73b2487d99095
damirchi <damirchi@pro.ai>
2023-02-17 11:15:44 -0800

    recent list component

---
+++ src/modules/new-flow/components/recents/index.jsx
@@ -0,0 +1,14 @@
+import ComponentWithStyle from "./styles";
+import {Card} from "@nextui-org/react";
+
+const RecentRoadMap = () => {
+
+    return (
+        <ComponentWithStyle>
+            <Card className={'card'}>
+
+            </Card>
+        </ComponentWithStyle>
+    )
+};
+export default RecentRoadMap;
---
+++ src/modules/new-flow/components/recents/styles.js
@@ -0,0 +1,9 @@
+import { styled } from '@nextui-org/react';
+
+
+
+export const ComponentWithStyle = styled('div', {
+    height: '100%'
+});
+
+export default ComponentWithStyle;
