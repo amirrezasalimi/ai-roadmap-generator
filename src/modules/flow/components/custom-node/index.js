d3f3538b1b861d5876559533a186f76aadae36bb
damirchi <damirchi@pro.ai>
2023-02-17 03:04:44 -0800

    add custom node

--- src/modules/flow/components/custom-node/index.js
+++ src/modules/flow/components/custom-node/index.js
@@ -1,16 +1,37 @@
 import React, { memo } from 'react';
 import { Handle } from 'reactflow';
 import styles from './styles.module.css';
+import clsx from 'clsx';
+import {log} from "util";
 
+export default memo(({ data: {label, level}, sourcePosition, targetPosition, ...props }) => {
+    let classNames = [];
+    if(level === 0){
+        classNames.push(styles.nodeCustomLevel0);
+    }
+    else if(level === 1){
+        classNames.push(styles.nodeCustomLevel1);
+    }
+    else {
+        classNames.push(styles.nodeCustomLevelOthers);
+    }
 
-export default memo(({ data }) => {
+    const returnHandleClassName = (position) => {
+        if(position === sourcePosition || position === targetPosition){
+            return styles.handle
+        }
+        else {
+            return  styles.handleDeActive
+        }
+    }
+
     return (
-        <div className={styles.nodeCustom}>
-            {data.label}
-            <Handle type="source" position="top" id="a" />
-            <Handle type="source" position="right" id="b" />
-            <Handle type="source" position="bottom" id="c" />
-            <Handle type="source" position="left" id="d" />
+        <div className={clsx(classNames)}>
+            {label}
+            <Handle className={returnHandleClassName("top")} type="source" position="top" id="a" />
+            <Handle className={returnHandleClassName("right")} type="source" position="right" id="b" />
+            <Handle className={returnHandleClassName("bottom")} type="source" position="bottom" id="c" />
+            <Handle className={returnHandleClassName("left")} type="source" position="left" id="d" />
         </div>
     );
 });
