38e2beb5104925dd13eb3460be316a853877fbff
seville <amirrezasalimi0@gmail.com>
2023-03-09 11:51:06 +0330

    chore: no ssr for new roadmap

--- src/modules/new-flow/index.jsx
+++ src/modules/new-flow/index.jsx
@@ -1,5 +1,5 @@
 import Button from "@/shared/components/button";
-import {Row, Text, Grid, Spacer} from "@nextui-org/react";
+import { Row, Text, Grid, Spacer } from "@nextui-org/react";
 import Image from "next/image";
 import ComponentWithStyle from "./styles";
 import RecentRoadMap from "@/modules/new-flow/components/recents";
@@ -9,13 +9,14 @@ import Link from "next/link";
 import APP from "@/shared/constants/app";
 
 const NewFlow = () => {
+    const isBrowser = typeof window != "undefined"
     return (
         <ComponentWithStyle>
-            <Spacer y={2}/>
+            <Spacer y={2} />
             <Row justify={'center'}>
                 <Logo size={"lg"} />
             </Row>
-            <Spacer y={2}/>
+            <Spacer y={2} />
             <Grid.Container className={'note'}>
                 <Grid xs={12} sm={10} md={8} lg={6} xl={5} display="flex" direction="column" justify="center">
                     <div className={"card"}>
@@ -26,11 +27,11 @@ const NewFlow = () => {
                     </div>
                 </Grid>
             </Grid.Container>
-            <Spacer y={1}/>
+            <Spacer y={1} />
             <Grid.Container className={'content'}>
                 <Grid xs={12} sm={5} md={4} lg={3} xl={2.5} display="flex" justify="center">
                     <div className={'box'}>
-                        <FormNewRoadMap />
+                        {isBrowser && <FormNewRoadMap />}
                     </div>
                 </Grid>
                 <Grid xs={12} sm={5} md={4} lg={3} xl={2.5} display="flex" justify="center">
@@ -42,20 +43,20 @@ const NewFlow = () => {
             <Row className="buttonLinkContainer">
                 <Link href={APP.GITHUB_LINK}>
                     <Button size={'lg'}>
-                        <Image className={"icon"} alt="github" height={25} width={25} src={'github.svg'}/>
+                        <Image className={"icon"} alt="github" height={25} width={25} src={'github.svg'} />
                         github
                     </Button>
                 </Link>
                 <Link href={APP.FIGMA_LINK}>
                     <Button size={'lg'}>
-                        <Image className={"icon"} alt="figma" height={25} width={25} src={'figma.svg'}/>
+                        <Image className={"icon"} alt="figma" height={25} width={25} src={'figma.svg'} />
                         figma
                     </Button>
                 </Link>
             </Row>
-            <Spacer y={2}/>
-            <div className="gradient1"/>
-            <div className="gradient2"/>
+            <Spacer y={2} />
+            <div className="gradient1" />
+            <div className="gradient2" />
         </ComponentWithStyle>
     )
 };
