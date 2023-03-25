1b7817f18092ff324654c9d7be3a7f4851fc5b57
damirchi <damirchi@pro.ai>
2023-03-25 11:50:32 -0700

    update next ui styles

--- src/infrastructure/next-ui.js
+++ src/infrastructure/next-ui.js
@@ -4,17 +4,24 @@ export const defaultTheme = createTheme({
     type: 'dark',
     theme: {
       colors: {
-        background: '#000',
-        primary: "#000",
-        secondary: "",
+        background: '#0C0C0C',
+        paper: '#141414',
+        primary: "#141414",
+        secondary: "#343434",
         primaryText: '#fff',
-        borderColor: "#001E38",
-        inputBorderColor: "red",
+        borderColor: "#343434",
         secondaryText: "#6987C6",
         tertiaryText: '#B0C4EF',
         gradient: "linear-gradient(268.81deg, #1A76DB 9.85%, #8A3BEF 99.6%)",
-        gradientText: "268.81deg, #1A76DB 1.85%, #8A3BEF 80.6%"
+        gradientText: "268.81deg, #1A76DB 1.85%, #8A3BEF 80.6%",
       },
+      lineHeights: {
+        xs: 1,
+        sm: 1.25,
+        base: 1.25,
+        md: 1.25,
+        lg: 1.25,
+        xl: 1.25,
+      }
     },
   })
-  
