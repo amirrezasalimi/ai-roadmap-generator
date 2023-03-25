343847ce361475ab47082fec191f4ca4a7d6e572
damirchi <damirchi@pro.ai>
2023-03-25 11:55:07 -0700

    Update button component style

--- src/shared/components/button/index.js
+++ src/shared/components/button/index.js
@@ -1,10 +1,24 @@
 import { Button as NextButton, styled } from '@nextui-org/react';
 
-
-
 const Button = styled(NextButton, {
-      borderRadius: 5,
-      minWidth: 'unset !important'
+      borderRadius: 8,
+      borderColor: "$borderColor",
+      color: "#fff",
+      minWidth: 'unset !important',
+      variants: {
+          color: {
+              primary: {
+                  borderColor: "$borderColor",
+                  color: "#fff",
+              }
+          },
+          bordered: {
+              true: {
+                  border: "1px solid $borderColor",
+                  color: "#fff",
+              }
+          }
+      }
     });
 
 export default Button;
