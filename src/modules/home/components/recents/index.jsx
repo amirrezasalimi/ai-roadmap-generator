9c5a3c2f25ef6a100396b697371e0255891d14d5
damirchi <damirchi@pro.ai>
2023-04-01 07:33:01 -0700

    update recents and recent item

--- src/modules/home/components/recents/index.jsx
+++ src/modules/home/components/recents/index.jsx
@@ -1,10 +1,7 @@
-import ComponentWithStyle from "./styles";
-import {Card, Loading, Row, Text} from "@nextui-org/react";
-import Image from "next/image";
-import Link from "next/link";
-import makeUrl from "@/shared/helper/make-url";
-import {LINKS} from "@/shared/constants/links";
 import {useEffect} from "react";
+import ComponentWithStyle from "./styles";
+import RecentItem from "./components/recent-item";
+import {Card, Loading, Row, Text} from "@nextui-org/react";
 import useGetRecentRoadmap from "@/modules/home/hooks/get-recent-roadmap";
 
 const RecentRoadMap = () => {
@@ -29,14 +26,7 @@ const RecentRoadMap = () => {
                     {
                         hookGetRecentRoadmap.status === "done" &&
                         hookGetRecentRoadmap?.data?.map((item)=> (
-                            <Link target={'_blank'} key={`recent-roadmap-item-${item.code}`} href={makeUrl(LINKS.ROADMAP, {id: item.code})}>
-                                <div className={"item"}>
-                                    <Image className={"icon"} alt="github" height={25} width={25} src={'link.svg'}/>
-                                    <Text className={"text"}>
-                                        {item.title}
-                                    </Text>
-                                </div>
-                            </Link>
+                            <RecentItem roadmap={item} />
                         ))
                     }
 
