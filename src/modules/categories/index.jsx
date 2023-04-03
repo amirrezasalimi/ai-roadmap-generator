f3f4053a2eff79af07c1c4354d7ab2a6e739c85a
damirchi <damirchi@pro.ai>
2023-04-02 19:37:35 -0700

    add category title on header

--- src/modules/categories/index.jsx
+++ src/modules/categories/index.jsx
@@ -1,14 +1,39 @@
 import ComponentWithStyle from "./styles";
-import {Row, Spacer} from "@nextui-org/react";
+import {Row, Spacer, Text} from "@nextui-org/react";
 import Logo from "@/shared/components/logo";
-
+import Button from "@/shared/components/button";
+import Image from "next/image";
+import {useRouter} from "next/navigation";
 const Categories = () => {
-
+  const router = useRouter();
+  const backEvent = () => {
+      router.back()
+  }
   return (
-    <ComponentWithStyle>
+    <ComponentWithStyle md>
       <Spacer y={2} />
       <Row justify={'center'}>
         <Logo size={"lg"} />
+      </Row>
+      <Spacer y={2} />
+      <Row>
+          <Button onClick={backEvent} className={"backButton"} color={"default"} bordered>
+              <Image className={"icon"} alt="back" height={25} width={25} src={'arrow-left.svg'} />
+          </Button>
+          <div className={"headerCategory"}>
+              <div className={"iconContainer"}>
+                  <Image className={"icon"} alt="back" height={60} width={60} src={'category-icon/product-management.svg'} />
+                  <div className={"gradient"} />
+              </div>
+              <div className={"textContainer"}>
+                  <Text size={16} className={"text"}>
+                      Product management
+                  </Text>
+                  <Text size={16} className={"count"}>
+                      12 <span>/Roadmap</span>
+                  </Text>
+              </div>
+          </div>
       </Row>
     </ComponentWithStyle>
   )
--- src/modules/categories/styles.js
+++ src/modules/categories/styles.js
@@ -1,7 +1,44 @@
 import {Container, styled} from '@nextui-org/react';
 
 export const ComponentWithStyle = styled(Container, {
+    '.backButton': {
+        height: '56px',
+        'icon': {
 
+        }
+    },
+    '.headerCategory': {
+        display: "flex",
+        margin: "auto",
+        alignItems: "center",
+        flexDirection: "column",
+        '.iconContainer': {
+            position: "relative",
+            '.icon': {
+                color: 'red',
+            },
+            '.gradient': {
+                bottom: 0,
+                height: 64,
+                position: "absolute",
+                width: "100%",
+                backgroundImage: "linear-gradient(rgb(12 12 12 / 46%), rgb(12, 12, 12))"
+            },
+        },
+        ".textContainer": {
+            top: 45,
+            position: "absolute",
+            ".text": {
+                textAlign: "center",
+            },
+            ".count": {
+                textAlign: "center",
+                "span": {
+                    color: "#515151"
+                }
+            }
+        }
+    }
 });
 
 export default ComponentWithStyle;
