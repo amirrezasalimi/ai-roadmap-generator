aeca1214ca7098df608342ce7587bd84a7991cea
damirchi <damirchi@pro.ai>
2023-04-02 18:33:16 -0700

    add header

--- src/modules/categories/index.jsx
+++ src/modules/categories/index.jsx
@@ -1,10 +1,15 @@
 import ComponentWithStyle from "./styles";
+import {Row, Spacer} from "@nextui-org/react";
+import Logo from "@/shared/components/logo";
 
 const Categories = () => {
 
   return (
     <ComponentWithStyle>
-      
+      <Spacer y={2} />
+      <Row justify={'center'}>
+        <Logo size={"lg"} />
+      </Row>
     </ComponentWithStyle>
   )
 };
