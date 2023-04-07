11459d9aa8dd8feb2f94bfd2fc06da0b1e1f7eba
damirchi <damirchi@pro.ai>
2023-04-07 08:04:12 -0700

    create category card component home page

--- src/modules/home/components/categories/index.jsx
+++ src/modules/home/components/categories/index.jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import {useEffect} from "react";
 import ComponentWithStyle from "./styles";
-import makeUrl from "@/shared/helper/make-url";
 import {LINKS} from "@/shared/constants/links";
 import {Card, Loading, Row, Text} from "@nextui-org/react";
 import useGetCategories from "@/shared/hooks/get-categories";
 import Button from "@/shared/components/button";
+import CategoryCard from "./components/category-card";
 
 const Categories = () => {
     const hookGetCategories = useGetCategories();
@@ -38,17 +38,7 @@ const Categories = () => {
                     {
                         hookGetCategories.status === "done" &&
                         hookGetCategories?.data?.map((item)=> (
-                            <Link target={'_blank'} key={`recent-roadmap-item-${item.code}`} href={makeUrl(LINKS.ROADMAP, {id: item.code})}>
-                                <div className={"item"}>
-                                    <Image className={"icon"} alt="github" height={20} width={20} src={'link.svg'}/>
-                                    <Text size={16} className={"text"}>
-                                        {item.title}
-                                    </Text>
-                                    <Text size={16} className={"count"}>
-                                        {item.count} <span>/Roadmap</span>
-                                    </Text>
-                                </div>
-                            </Link>
+                            <CategoryCard data={item} />
                         ))
                     }
 
