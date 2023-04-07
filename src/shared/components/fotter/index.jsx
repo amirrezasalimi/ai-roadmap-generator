50d0b757b1901ea5404b9997c1628180874c862a
damirchi <damirchi@pro.ai>
2023-04-07 04:15:56 -0700

    add footer component

---
+++ src/shared/components/fotter/index.jsx
@@ -0,0 +1,10 @@
+import ComponentWithStyle from "./styles";
+
+const Footer = (props) => {
+    return (
+        <ComponentWithStyle>
+
+        </ComponentWithStyle>
+    )
+};
+export default Footer;
---
+++ src/shared/components/fotter/styles.js
@@ -0,0 +1,6 @@
+import { styled } from '@nextui-org/react';
+export const ComponentWithStyle = styled('div', {
+
+});
+
+export default ComponentWithStyle;
