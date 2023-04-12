7cb19822d1106b1334d3655be4f529fb9c8173f0
damirchi <damirchi@pro.ai>
2023-04-12 15:41:34 -0700

    add created date and likes into the roadmap component

--- src/modules/category-item/components/roadmap-card/index.jsx
+++ src/modules/category-item/components/roadmap-card/index.jsx
@@ -4,29 +4,32 @@ import ComponentWithStyle from "./styles";
 import makeUrl from "@/shared/helper/make-url";
 import { LINKS } from "@/shared/constants/links";
 import  { Text } from "@nextui-org/react";
+import ReactTimeAgo from "react-time-ago";
 
 const RoadmapCard = ( { data }) => {
     return (
         <ComponentWithStyle>
             <Link target={'_blank'} key={`recent-roadmap-item-${data.code}`} href={makeUrl(LINKS.ROADMAP, {id: data.code})}>
-                <div className={"item"}>
                     <div className={"content"}>
-                        <Image className={"icon"} alt="link" height={16} width={16} src={'link.svg'}/>
+                        <Image className={"icon"} alt="link" height={16} width={16} src={'/link.svg'}/>
                         <Text className={"text"}>
                             {data.title}
                         </Text>
                     </div>
                     <div className={"details"}>
-                        <div className={"counters"}>
-                            <Text className={"text"}>
-                                1 days ago
-                            </Text>
-                            <Text className={"text"}>
-                                54 likes
-                            </Text>
-                        </div>
+                        <Text className={"text"}>
+                            {data?.created && (
+                                <ReactTimeAgo
+                                    verboseDate={new Date().toISOString()}
+                                    date={new Date(data?.created ?? "")}
+                                    locale="en-US"
+                                />
+                            )}
+                        </Text>
+                        <Text className={"text"}>
+                            {data.likes} likes
+                        </Text>
                     </div>
-                </div>
             </Link>
         </ComponentWithStyle>
     )
--- src/modules/category-item/components/roadmap-card/styles.js
+++ src/modules/category-item/components/roadmap-card/styles.js
@@ -2,48 +2,49 @@ import {styled} from '@nextui-org/react';
 
 
 export const ComponentWithStyle = styled('fragment', {
-    ".item": {
+    width: "100%",
+    height: 110,
+    border: "1px solid #343434",
+    borderRadius: 8,
+    background: "$paper",
+    ".content": {
+        justifyContent: "flex-start",
+        padding: 16,
+        height: 76,
         width: "100%",
-        border: "1px solid #343434",
-        borderRadius: 8,
-        ".content": {
-            justifyContent: "flex-start",
-            padding: 16,
-            width: "100%",
-            display: "flex",
-            borderBottom: "1px solid #343434",
-            ".text": {
-                textAlign: "justify",
-            },
-            ".icon": {
-
-            },
+        display: "flex",
+        borderBottom: "1px solid #343434",
+        ".text": {
+            display: "-webkit-box",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            "-webkit-line-clamp": 2,
+            "-webkit-box-orient": "vertical",
         },
-        ".details": {
-            padding: 8,
-            width: "100%",
-            display: "flex",
-            flexWrap: "wrap",
-            gap: 8,
+        ".icon": {
+            marginRight: 8
+        },
+    },
+    ".details": {
+        height: 32,
+        padding: 8,
+        width: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 8,
+        "@sm": {
+            flexWrap: "nowrap",
+            gap: 0,
+        },
+        justifyContent: "space-between",
+        alignItems: "center",
+        '.text': {
+            fontSize: 14,
+            color: "#515151",
             "@sm": {
-                flexWrap: "nowrap",
-                gap: 0,
+                fontSize: 12,
             },
-            justifyContent: "space-between",
-            '.counters': {
-                display: "flex",
-                alignItems: "center",
-                gap: 8,
-                '.text': {
-                    fontSize: 14,
-                    color: "#515151",
-                    "@sm": {
-                        fontSize: 12,
-                    },
-                },
-            }
         },
-
     },
 });
 
