9138d7f3e6df152754aae034b60b29470e4fad60
seville <amirrezasalimi0@gmail.com>
2023-06-05 01:09:58 +0330

    chore: add basepath option

--- src/shared/constants/config.js
+++ src/shared/constants/config.js
@@ -1,4 +1,5 @@
 export const OPEN_AI_TOKEN = process.env.OPEN_AI_TOKEN
+export const OPEN_AI_BASEPATH = process.env.OPEN_AI_BASEPATH
 export const POCKETBASE_URL = process.env.POCKETBASE_URL
 export const POCKETBASE_ADMIN_EMAIL = process.env.POCKETBASE_ADMIN_EMAIL
 export const POCKETBASE_ADMIN_PASSWORD = process.env.POCKETBASE_ADMIN_PASSWORD
--- src/shared/helper/openai.js
+++ src/shared/helper/openai.js
@@ -1,9 +1,10 @@
 import { Configuration, OpenAIApi } from "openai"
-import { OPEN_AI_TOKEN } from "../constants/config"
+import { OPEN_AI_TOKEN, OPEN_AI_BASEPATH } from "../constants/config"
 
 export default function openAiInstance(apiKey = OPEN_AI_TOKEN) {
     const openai_config = new Configuration({
-        apiKey
+        apiKey,
+        basePath: OPEN_AI_BASEPATH || undefined
     })
     return new OpenAIApi(openai_config)
 }
