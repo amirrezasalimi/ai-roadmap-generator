c62dabb058a8cce325c42749ed0bf234d70646b4
seville <amirrezasalimi0@gmail.com>
2023-06-08 01:54:03 +0330

    chore: allow cors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)?',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+          },
+        ]
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
